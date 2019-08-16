02d3f497c3b76d4ae8c369273cc97b859ed53a62
Hidetaka Sasai <sasai@ds-solution.co.jp>
2019-08-16 15:10:51 +0900

    Feature db.

--- src/main/java/to/kit/program/SecurityConfig.java
+++ src/main/java/to/kit/program/SecurityConfig.java
@@ -1,8 +1,13 @@
 package to.kit.program;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
@@ -11,15 +16,41 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @Configuration
 @ConfigurationProperties(prefix = "security")
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private DataSource dataSource;
+
 	private boolean requiresSecure;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/", "/h2-console/**").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin().loginProcessingUrl("/logon").permitAll();
 		http.csrf().disable();
+		http.headers().frameOptions().disable();
 		if (this.requiresSecure) {
 			http.requiresChannel().anyRequest().requiresSecure();
 		}
+	}
+
+	/**
+	 * Authenticate.
+	 * @param auth AuthenticationManagerBuilder
+	 * @throws Exception exception
+	 */
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//		UserBuilder users = User.withDefaultPasswordEncoder();
+
+		auth.jdbcAuthentication().dataSource(this.dataSource)
+//			.withDefaultSchema()
+//			.withUser(users.username("user").password("password").roles("USER"))
+//			.withUser(users.username("admin").password("password").roles("USER", "ADMIN"))
+		;
 	}
 
 	/**
