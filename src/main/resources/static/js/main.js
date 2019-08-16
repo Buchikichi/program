554666f019fbf9e76febcb4d63ed4caf77d0e5eb
Hidetaka Sasai <sasai@ds-solution.co.jp>
2019-08-16 17:53:54 +0900

    Feature checkTree.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -7,15 +7,30 @@ document.addEventListener('DOMContentLoaded', ()=> {
 	$('#tree').jstree({
 		core: {
 			dblclick_toggle: false,
+			check_callback: checkTree,
+			themes: {
+				icons: false,
+				dots: false,
+				responsive: true,
+				variant: 'large',
+			}
 		},
 		dnd: {
 			is_draggable: true,
 		},
-		plugins: ['checkbox', 'dnd'],
+		plugins: ['dnd'],
 	});
 	$('#tree').jstree('open_all');
-//	$('li').draggable();
-//	$('li.element').selectable();
-//	$('.root > li').sortable();
-//	$('li').sortable();
 });
+
+function checkTree(operation, node, node_parent, node_position, more) {
+	if (operation !== 'move_node') {
+		return;
+	}
+	if (node_parent.li_attr == null) {
+		return false;
+	}
+	let clazz = node_parent.li_attr.class;
+
+	return clazz.indexOf('process') == -1;
+}
